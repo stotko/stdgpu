0420820907b152e808599312bb06722a75bc95ec
Patrick Stotko <stotko@cs.uni-bonn.de>
2019-10-26 19:54:37 +0200

    cmake: Improve performance of CUDA compute capability detection

--- cmake/check_compute_capability.cmake
+++ cmake/check_compute_capability.cmake
@@ -1,14 +1,21 @@
 # Check for GPUs present and their compute capability
 # based on http://stackoverflow.com/questions/2285185/easiest-way-to-test-for-existence-of-cuda-capable-gpu-from-cmake/2297877#2297877 (Christopher Bruns)
 
-set(STDGPU_CUDA_COMPUTE_CAPABILITIES_SOURCE "${CMAKE_CURRENT_LIST_DIR}/cuda_compute_capability.cu")
+set(STDGPU_CUDA_COMPUTE_CAPABILITIES_SOURCE "${CMAKE_CURRENT_LIST_DIR}/cuda_compute_capability.cpp")
 message(STATUS "Detecting CCs of GPUs : ${STDGPU_CUDA_COMPUTE_CAPABILITIES_SOURCE}")
 
+# Detect CUDA runtime library to build the .cpp file (implicitly used if .cu file was used)
+find_library(STDGPU_CUDART_LIBRARY cudart
+             HINTS ${CMAKE_CUDA_IMPLICIT_LINK_DIRECTORIES})
+
 try_run(STDGPU_RUN_RESULT_VAR STDGPU_COMPILE_RESULT_VAR
-    ${CMAKE_BINARY_DIR}
-    ${STDGPU_CUDA_COMPUTE_CAPABILITIES_SOURCE}
-    COMPILE_OUTPUT_VARIABLE STDGPU_COMPILE_OUTPUT_VAR
-    RUN_OUTPUT_VARIABLE STDGPU_RUN_OUTPUT_VAR)
+        ${CMAKE_BINARY_DIR}
+        ${STDGPU_CUDA_COMPUTE_CAPABILITIES_SOURCE}
+        CMAKE_FLAGS
+            "-DINCLUDE_DIRECTORIES=${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES}"
+            "-DLINK_LIBRARIES=${STDGPU_CUDART_LIBRARY}"
+        COMPILE_OUTPUT_VARIABLE STDGPU_COMPILE_OUTPUT_VAR
+        RUN_OUTPUT_VARIABLE STDGPU_RUN_OUTPUT_VAR)
 
 # COMPILE_RESULT_VAR is TRUE when compile succeeds
 # RUN_RESULT_VAR is zero when a GPU is found
