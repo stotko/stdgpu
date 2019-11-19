aa01e504908595deb56504ba107cac7689c0a2ba
Patrick Stotko <stotko@cs.uni-bonn.de>
2019-11-19 13:30:00 +0100

    test: Split backend-specific device information code into dedicated file

--- test/stdgpu/main.cpp
+++ test/stdgpu/main.cpp
@@ -17,33 +17,12 @@
 
 #include <algorithm>
 #include <cstdio>
-#include <cuda_runtime_api.h>
+#include <string>
 
+#include <stdgpu/cuda/device_info.h>
 #include <stdgpu/config.h>
 #include <stdgpu/memory.h>
 
-
-
-namespace detail
-{
-    float
-    kilo_to_mega_hertz(const float kilo_hertz)
-    {
-        return kilo_hertz / 1000.0f;
-    }
-
-    float
-    byte_to_kibi_byte(const float byte)
-    {
-        return byte / 1024.0f;
-    }
-
-    float
-    byte_to_gibi_byte(const float byte)
-    {
-        return byte / (1024.0f * 1024.0f * 1024.0f);
-    }
-}
 
 
 GTEST_API_ int
@@ -68,34 +47,7 @@ main(int argc, char* argv[])
 
 
 
-    // Print GPU information
-    cudaDeviceProp properties;
-    cudaGetDeviceProperties( &properties, 0 );
-
-    size_t free_memory  = 0;
-    size_t total_memory = 0;
-    cudaMemGetInfo(&free_memory, &total_memory);
-
-    std::string gpu_name = properties.name;
-    int gpu_name_total_width = 57;
-    int gpu_name_size = static_cast<int>(gpu_name.size());
-    int gpu_name_space_left  = std::max<int>(1, (gpu_name_total_width - gpu_name_size) / 2);
-    int gpu_name_space_right = std::max<int>(1, gpu_name_total_width - gpu_name_size - gpu_name_space_left);
-
-    printf( "+---------------------------------------------------------+\n" );
-    printf( "|%*s%*s%*s|\n", gpu_name_space_left, " ", gpu_name_size, gpu_name.c_str(), gpu_name_space_right, " ");
-    printf( "+---------------------------------------------------------+\n" );
-    printf( "| Compute Capability        :   %1d.%1d                       |\n", properties.major, properties.minor );
-    printf( "| Clock rate                :   %-6.0f MHz                |\n", detail::kilo_to_mega_hertz(properties.clockRate));
-    printf( "| Global Memory             :   %-6.3f GiB / %-6.3f GiB   |\n", detail::byte_to_gibi_byte(free_memory), detail::byte_to_gibi_byte(total_memory));
-    printf( "| Memory Bus Width          :   %-6d Bit                |\n", properties.memoryBusWidth );
-    printf( "| Multiprocessor (SM) count :   %-6d                    |\n", properties.multiProcessorCount );
-    printf( "| Warp size                 :   %-6d Threads            |\n", properties.warpSize );
-    printf( "| L2 Cache                  :   %-6.0f KiB                |\n", detail::byte_to_kibi_byte(properties.l2CacheSize));
-    printf( "| Total Constant Memory     :   %-6.0f KiB                |\n", detail::byte_to_kibi_byte(properties.totalConstMem));
-    printf( "| Shared Memory per SM      :   %-6.0f KiB                |\n", detail::byte_to_kibi_byte(properties.sharedMemPerMultiprocessor));
-    printf( "| Total Shared Memory       :   %-6.0f KiB                |\n", detail::byte_to_kibi_byte(properties.sharedMemPerMultiprocessor * static_cast<size_t>(properties.multiProcessorCount)));
-    printf( "+---------------------------------------------------------+\n\n" );
+    stdgpu::cuda::print_device_information();
 
 
 
