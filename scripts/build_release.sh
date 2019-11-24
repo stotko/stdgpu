da7434d02fd0fe0915cdbe0bbc48ff3afdb8dcad
Patrick Stotko <stotko@cs.uni-bonn.de>
2019-11-24 14:31:48 +0100

    scripts: Improve console output and internal structure

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
 
 # Build project
+cmake -E cmake_echo_color --blue ">>>>> Build stdgpu project"
 cmake --build build --config Release --parallel 13
--- scripts/create_documentation.sh
+++ scripts/create_documentation.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
 
 # Create and install documentation
+cmake -E cmake_echo_color --blue ">>>>> Create documentation"
 cmake --build build --target stdgpu_doc
