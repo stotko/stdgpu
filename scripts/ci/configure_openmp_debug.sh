f6315ac746d513c7cc902cc54c79487f7977beaf
Patrick Stotko <stotko@cs.uni-bonn.de>
2019-12-01 18:15:19 +0100

    CI: Split project building into separate step

--- scripts/ci/configure_openmp_debug.sh
+++ scripts/ci/configure_openmp_debug.sh
@@ -8,7 +8,4 @@ sh scripts/utils/create_empty_directory.sh build
 sh scripts/utils/download_dependencies.sh
 
 # Configure project
-sh scripts/utils/configure_release.sh -DSTDGPU_BACKEND=STDGPU_BACKEND_OPENMP -Dthrust_ROOT=external/thrust
-
-# Build project
-sh scripts/build_release.sh
+sh scripts/utils/configure_debug.sh -DSTDGPU_BACKEND=STDGPU_BACKEND_OPENMP -Dthrust_ROOT=external/thrust
