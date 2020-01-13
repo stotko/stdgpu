c11b4cf1616f87b650e408b18d9e3ae3a037dbec
Patrick Stotko <stotko@cs.uni-bonn.de>
2020-01-13 15:51:06 +0100

    scripts: Port install script to native CMake install command-line interface

--- scripts/install_release.sh
+++ scripts/install_release.sh
@@ -3,4 +3,4 @@ set -e
 
 # Install project
 cmake -E cmake_echo_color --blue ">>>>> Install stdgpu project"
-cmake -DCOMPONENT="stdgpu" -P build/cmake_install.cmake
+cmake --install build --config Release --component stdgpu
