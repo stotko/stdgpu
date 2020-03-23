0db787a8553f8ea94f5bee353ccc16c8721930a0
Patrick Stotko <stotko@cs.uni-bonn.de>
2020-03-23 12:29:44 +0100

    platform: Move definitions to backend-specific modules and add automatic dispatching

---
+++ src/stdgpu/cuda/platform.h
@@ -0,0 +1,96 @@
+/*
+ *  Copyright 2020 Patrick Stotko
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef STDGPU_CUDA_PLATFORM_H
+#define STDGPU_CUDA_PLATFORM_H
+
+
+#include <stdgpu/compiler.h>
+
+
+
+namespace stdgpu
+{
+namespace cuda
+{
+
+/**
+ * \def STDGPU_CUDA_HOST_DEVICE
+ * \hideinitializer
+ * \brief Platform-independent host device function annotation
+ */
+#if STDGPU_DEVICE_COMPILER == STDGPU_DEVICE_COMPILER_NVCC
+    #define STDGPU_CUDA_HOST_DEVICE __host__ __device__
+#else
+    #define STDGPU_CUDA_HOST_DEVICE
+#endif
+
+
+/**
+ * \def STDGPU_CUDA_DEVICE_ONLY
+ * \hideinitializer
+ * \brief Platform-independent device function annotation
+ */
+#if STDGPU_DEVICE_COMPILER == STDGPU_DEVICE_COMPILER_NVCC
+    #define STDGPU_CUDA_DEVICE_ONLY __device__
+#else
+    // Should trigger a compact error message containing the error string
+    #define STDGPU_CUDA_DEVICE_ONLY sizeof("STDGPU ERROR: Wrong compiler detected! Device-only functions must be compiled with the device compiler!")
+#endif
+
+
+/**
+ * \def STDGPU_CUDA_CONSTANT
+ * \hideinitializer
+ * \brief Platform-independent constant variable annotation
+ */
+#if STDGPU_DEVICE_COMPILER == STDGPU_DEVICE_COMPILER_NVCC
+    #define STDGPU_CUDA_CONSTANT __constant__
+#else
+    #define STDGPU_CUDA_CONSTANT
+#endif
+
+
+/**
+ * \def STDGPU_CUDA_IS_DEVICE_CODE
+ * \hideinitializer
+ * \brief Platform-independent device code detection
+ */
+#if defined(__CUDA_ARCH__)
+    #define STDGPU_CUDA_IS_DEVICE_CODE 1
+#else
+    #define STDGPU_CUDA_IS_DEVICE_CODE 0
+#endif
+
+
+/**
+ * \def STDGPU_CUDA_IS_DEVICE_COMPILED
+ * \hideinitializer
+ * \brief Platform-independent device compilation detection
+ */
+#if STDGPU_DEVICE_COMPILER == STDGPU_DEVICE_COMPILER_NVCC
+    #define STDGPU_CUDA_IS_DEVICE_COMPILED 1
+#else
+    #define STDGPU_CUDA_IS_DEVICE_COMPILED 0
+#endif
+
+
+} // namespace cuda
+
+} // namespace stdgpu
+
+
+
+#endif // STDGPU_CUDA_PLATFORM_H
