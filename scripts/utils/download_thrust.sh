2f25182fd9eb88f1024742c47924984fe8ac9f68
Patrick Stotko <stotko@cs.uni-bonn.de>
2020-04-06 11:35:09 +0200

    scripts: Use released thrust version

--- scripts/utils/download_thrust.sh
+++ scripts/utils/download_thrust.sh
@@ -3,3 +3,6 @@ set -e
 
 cmake -E cmake_echo_color --blue ">>>>> Download thrust"
 cmake -E chdir external git clone https://github.com/thrust/thrust
+
+# Latest release version
+cmake -E chdir external/thrust git checkout 1.9.8
