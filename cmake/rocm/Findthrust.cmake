2261c1934cf3430b2e519257d968d2f79d9acfee
Patrick Stotko <stotko@cs.uni-bonn.de>
2020-04-14 15:29:54 +0200

    cmake: Workaround bug in imported rocthrust target name

--- cmake/rocm/Findthrust.cmake
+++ cmake/rocm/Findthrust.cmake
@@ -43,7 +43,12 @@ find_package_handle_standard_args(thrust
 
 if(thrust_FOUND)
     add_library(thrust::thrust INTERFACE IMPORTED)
-    set_target_properties(thrust::thrust PROPERTIES INTERFACE_LINK_LIBRARIES rocthrust)
+    # WORKAROUND: ROCm 3.1 forgot to export the namespace, so workaround this bug
+    if(TARGET roc::rocthrust)
+        set_target_properties(thrust::thrust PROPERTIES INTERFACE_LINK_LIBRARIES roc::rocthrust)
+    elseif(TARGET rocthrust)
+        set_target_properties(thrust::thrust PROPERTIES INTERFACE_LINK_LIBRARIES rocthrust)
+    endif()
 
     mark_as_advanced(THRUST_INCLUDE_DIR
                      THRUST_VERSION
