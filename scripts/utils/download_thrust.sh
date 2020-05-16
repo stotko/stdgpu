e5da285727d303a17cf7ee9c64620172dec4da5a
Patrick Stotko <stotko@cs.uni-bonn.de>
2020-05-16 20:11:11 +0200

    cmake: Fix parsing of thrust version

--- scripts/utils/download_thrust.sh
+++ scripts/utils/download_thrust.sh
@@ -5,4 +5,4 @@ cmake -E cmake_echo_color --blue ">>>>> Download thrust"
 cmake -E chdir external git clone https://github.com/thrust/thrust
 
 # Latest release version
-cmake -E chdir external/thrust git checkout 1.9.8
+cmake -E chdir external/thrust git checkout 1.9.10
