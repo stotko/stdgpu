5751956a8966477207c512a7ea1d1e37d033c7ee
Patrick Stotko <stotko@cs.uni-bonn.de>
2020-12-22 15:45:32 +0100

    General: Move CI to GitHub Actions

--- scripts/utils/download_thrust.sh
+++ scripts/utils/download_thrust.sh
@@ -2,7 +2,4 @@
 set -e
 
 cmake -E cmake_echo_color --blue ">>>>> Download thrust"
-cmake -E chdir external git clone https://github.com/thrust/thrust
-
-# Latest release version
-cmake -E chdir external/thrust git checkout 1.9.10
+cmake -E chdir external git clone https://github.com/NVIDIA/thrust
