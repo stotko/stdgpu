1c91901dec17189b58d3363b30ac47ec385a025e
Patrick Stotko <stotko@cs.uni-bonn.de>
2021-07-18 15:13:04 +0200

    General: Port HIP backend to CMake HIP language

--- cmake/hip/set_device_flags.cmake
+++ cmake/hip/set_device_flags.cmake
@@ -2,10 +2,30 @@ function(stdgpu_set_device_flags STDGPU_OUTPUT_DEVICE_FLAGS)
     # Clear list before appending flags
     unset(${STDGPU_OUTPUT_DEVICE_FLAGS})
 
-    list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wno-unused-command-line-argument")
-    list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wno-pass-failed")
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+        list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wall")
+        list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wextra")
+        list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wshadow")
+        list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wsign-compare")
+        list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wconversion")
+        list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wfloat-equal")
+        list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wundef")
+        list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wdouble-promotion")
 
-    set(${STDGPU_OUTPUT_DEVICE_FLAGS} "$<$<COMPILE_LANGUAGE:CXX>:${${STDGPU_OUTPUT_DEVICE_FLAGS}}>")
+        if(STDGPU_TREAT_WARNINGS_AS_ERRORS)
+            list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Werror")
+        endif()
+
+        if(CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+            list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-O3")
+        endif()
+    endif()
+
+    if(CMAKE_HIP_COMPILER_ID STREQUAL "ROCMClang")
+        list(APPEND ${STDGPU_OUTPUT_DEVICE_FLAGS} "-Wno-pass-failed")
+    endif()
+
+    set(${STDGPU_OUTPUT_DEVICE_FLAGS} "$<$<COMPILE_LANGUAGE:HIP>:${${STDGPU_OUTPUT_DEVICE_FLAGS}}>")
 
     # Make output variable visible
     set(${STDGPU_OUTPUT_DEVICE_FLAGS} ${${STDGPU_OUTPUT_DEVICE_FLAGS}} PARENT_SCOPE)
