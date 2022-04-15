0c17ee5c9315ec942bc055ae38ba01c928b1575e
Patrick Stotko <stotko@cs.uni-bonn.de>
2022-04-15 17:42:56 +0200

    test: Fix printing of properties for invalid devices

--- test/stdgpu/cuda/device_info.cpp
+++ test/stdgpu/cuda/device_info.cpp
@@ -55,7 +55,16 @@ void
 print_device_information()
 {
     cudaDeviceProp properties;
-    cudaGetDeviceProperties(&properties, 0);
+    if (cudaGetDeviceProperties(&properties, 0) != cudaSuccess)
+    {
+        printf( "+---------------------------------------------------------+\n" );
+        printf( "|                   Invalid CUDA Device                   |\n" );
+        printf( "+---------------------------------------------------------+\n" );
+        printf( "| WARNING: Unable to fetch properties of invalid device!  |\n" );
+        printf( "+---------------------------------------------------------+\n\n" );
+
+        return;
+    }
 
     std::size_t free_memory  = 0;
     std::size_t total_memory = 0;
