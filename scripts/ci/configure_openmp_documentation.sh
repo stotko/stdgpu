ddb2e3af0fc8a75571e9d18cc37f386c816b6f21
Patrick Stotko <stotko@cs.uni-bonn.de>
2023-03-17 15:17:26 +0100

    cmake,scripts: Clean up doxygen detection

--- scripts/ci/configure_openmp_documentation.sh
+++ scripts/ci/configure_openmp_documentation.sh
@@ -5,4 +5,4 @@ set -e
 sh scripts/utils/create_empty_directory.sh build
 
 # Configure project
-sh scripts/utils/configure.sh Release -DSTDGPU_BACKEND=STDGPU_BACKEND_OPENMP -DSTDGPU_COMPILE_WARNING_AS_ERROR=ON -Dthrust_ROOT=external/thrust -DDoxygen_ROOT=external/doxygen/bin
+sh scripts/utils/configure.sh Release -DSTDGPU_BACKEND=STDGPU_BACKEND_OPENMP -DSTDGPU_COMPILE_WARNING_AS_ERROR=ON -Dthrust_ROOT=external/thrust
