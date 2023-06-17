58e7b6cb0c12344a6d132e9e746bd17c499f1df9
Patrick Stotko <stotko@cs.uni-bonn.de>
2023-06-17 17:53:59 +0200

    cmake: Add option to enable building the documentation

--- scripts/ci/configure_openmp_documentation.sh
+++ scripts/ci/configure_openmp_documentation.sh
@@ -5,4 +5,4 @@ set -e
 sh scripts/utils/create_empty_directory.sh build
 
 # Configure project
-sh scripts/utils/configure.sh Release -DSTDGPU_BACKEND=STDGPU_BACKEND_OPENMP -DSTDGPU_COMPILE_WARNING_AS_ERROR=ON -Dthrust_ROOT=external/thrust
+sh scripts/utils/configure.sh Release -DSTDGPU_BACKEND=STDGPU_BACKEND_OPENMP -DSTDGPU_BUILD_DOCUMENTATION=ON -DSTDGPU_COMPILE_WARNING_AS_ERROR=ON -Dthrust_ROOT=external/thrust
