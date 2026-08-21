668ad975afd5010cb9ec75fefe80214b6c6330b9
Billy Robert O'Neal III <bion@microsoft.com>
2026-08-21 14:13:10 -0700

    Move the change to `thrust::thrust`.

--- cmake/Findthrust.cmake
+++ cmake/Findthrust.cmake
@@ -60,6 +60,7 @@ if(thrust_FOUND)
 
     add_library(thrust::thrust INTERFACE IMPORTED)
     set_target_properties(thrust::thrust PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${THRUST_INCLUDE_DIRS}")
+    target_compile_options(thrust::thrust INTERFACE $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/Zc:preprocessor>)
 
     mark_as_advanced(THRUST_INCLUDE_DIR
                      THRUST_INCLUDE_DIR_VARS
